3c9879bc397cae95ca0a5c72c14e9ff9adfcee71
jramirez <jramirez@sqasquared.com>
2024-09-23 09:26:17 -0700

    Cosmos-19337

---
+++ features/groupedActivities.feature
@@ -0,0 +1,10 @@
+Feature: Grouped Activities
+
+    #19337
+    @groupedActivities
+    Scenario: Given On_{Win}_{Chrome} and On_Cosmos_Site and Signed_in_{StudyBA}_user and Draft_Study_{MyStudies} and Config_Alert_Off When Click_Grouped_activities_feature Then Verify_Grouped_activities_feature_page
+        Given the user is logged in as "STUDYBA1"
+        And create or select a case a study
+        And click "Grouped activities" feature nav menu option
+        And config alert is "off"
+        Then Grouped Activities feature page is empty
--- features/steps/groupedActivities_steps.py
+++ features/steps/groupedActivities_steps.py
@@ -1,4 +1,19 @@
 import re, time
 from behave import *
-from pages.Cosmos_PatientDetails import Cosmos_PatientDetails
+from pages.Cosmos_GroupedActivities import Cosmos_GroupedActivites
 from playwright.sync_api import expect
+
+@then('Grouped Activities feature page is empty')
+def verify_Grouped_Activities_is_Empty(context):
+    try:
+        # Instantiate Cosmos_GroupedActivities with the page object from the context
+        patientGA = Cosmos_GroupedActivites(context.page)
+        # Check if there are exisiting records, if so then delete all records
+        if(patientGA.check_if_record_exists()):
+            patientGA.delete_all_records()
+        patientGA.verify_Grouped_Activities_Empty_State()
+        
+    except Exception as e:
+        print(f"Test failed: {e}")
+        raise  # Raise the exception to ensure failure is reported
+        time.sleep(99999)  # Pause to keep the browser open
